68761e072c81c353eb378ed9554027772aeb1bb1
Arne Beer <arne@twobeer.de>
2013-01-25 17:38:02 +0100

    kleines Debug, Namenfix

--- src/main.lua
+++ src/main.lua
@@ -6,20 +6,20 @@ require("states/main")
 require("states/menu")
 
 
-GameState = class(StackHelper)
+GameState = class("GameState", StackHelper)
 
 function love.load()
 
     resources = class(Resources)
     
 
-    resources:addImmage("cutie0", 'data/gfx/cutie-0.png')
-    resources:addImmage("cutie1", 'data/gfx/cutie-1.png')
-    resources:addImmage("cutie2", 'data/gfx/cutie-2.png')
-    resources:addImmage("cutie3", 'data/gfx/cutie-3.png')
-    resources:addImmage("cutie4", 'data/gfx/cutie-4.png')
-    resources:addImmage("cutie5", 'data/gfx/cutie-5.png')
-    resources:addImmage("arena", 'data/gfx/arena.png')
+    resources:addImage("cutie0", 'data/gfx/cutie-0.png')
+    resources:addImage("cutie1", 'data/gfx/cutie-1.png')
+    resources:addImage("cutie2", 'data/gfx/cutie-2.png')
+    resources:addImage("cutie3", 'data/gfx/cutie-3.png')
+    resources:addImage("cutie4", 'data/gfx/cutie-4.png')
+    resources:addImage("cutie5", 'data/gfx/cutie-5.png')
+    resources:addImage("arena", 'data/gfx/arena.png')
 
     love.graphics.setMode(1000, 600, false, true, 0) -- Fensteroberfläche
 

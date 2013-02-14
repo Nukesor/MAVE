2295780d42571549b67c453a0634b1bcba96a8cf
Arne Beer <arne@twobeer.de>
2013-02-14 04:06:28 +0100

    Particlesize adjusted and prettified

--- src/objects/particles.lua
+++ src/objects/particles.lua
@@ -9,7 +9,7 @@ function Particles:__init()
 	self.hit:setSpeed(50, 100)
 	self.hit:setGravity(300, 400)
 	self.hit:setColors(170, 0, 0, 255, 136, 0, 0, 200)
-	self.hit:setSizes(0.7, 0.4)
+	self.hit:setSizes(0.3, 0.2)
 	self.hit:setPosition(0,0)
 	self.hit:setLifetime(0.25) -- Zeit die der Partikelstrahl anhält
 	self.hit:setParticleLife( 1, 1.5 ) -- setzt Lebenszeit in min-max
@@ -22,11 +22,11 @@ function Particles:__init()
 
 
 	self.bleeding = love.graphics.newParticleSystem (resources.images.blood1, 200)
-	self.bleeding:setEmissionRate(20)
-	-- self.bleeding:setSpeed()
+	self.bleeding:setEmissionRate(60)
+	self.bleeding:setSpeed(30)
 	self.bleeding:setGravity(300, 400)
 	self.bleeding:setColors(170, 0, 0, 255, 136, 0, 0, 200)
-	self.bleeding:setSizes(0.7, 0.4)
+	self.bleeding:setSizes(0.3, 0.2)
 	self.bleeding:setPosition(0,0)
 	self.bleeding:setLifetime(0.25)
 	self.bleeding:setParticleLife( 2, 3 )

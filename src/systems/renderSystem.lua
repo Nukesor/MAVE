ddff9777ce2f08497356b4af39205f432064fe26
Raffomania <raffomania@gmail.com>
2013-03-20 16:05:23 +0100

    added a shader demo in renderSystem

--- src/systems/renderSystem.lua
+++ src/systems/renderSystem.lua
@@ -3,12 +3,26 @@ require("core/system")
 
 RenderSystem = class("RenderSystem", System)
 
+function RenderSystem:__init()
+    self.targets = {}
+    self.invert = love.graphics.newPixelEffect([[
+        vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords)
+        {
+            vec4 effectiveColor = Texel(texture, texture_coords);
+            return vec4(1.0-effectiveColor.rgb, effectiveColor.a);
+        }
+    ]])
+end
+
 function RenderSystem:update()
 	love.graphics.setColor(255, 255, 255)
     for index, entity in ipairs(self.targets) do
         local drawable = entity:getComponent("Drawable")
         local pos = entity:getComponent("Position")
+        -- Enable for teh lulz
+        --love.graphics.setPixelEffect(self.invert)
         love.graphics.draw(drawable.image, pos.x, pos.y, drawable.r, drawable.sx, drawable.sy, drawable.ox, drawable.oy)
+        love.graphics.setPixelEffect()
     end
 end
 

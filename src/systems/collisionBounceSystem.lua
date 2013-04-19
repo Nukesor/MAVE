76500713ff81152ddf24114968736e25215db0cc
Arne Beer <arne@twobeer.de>
2013-04-19 04:30:59 +0200

    Now i am finally allowed to commit :(

--- src/systems/collisionBounceSystem.lua
+++ src/systems/collisionBounceSystem.lua
@@ -5,15 +5,6 @@ function CollisionBounceSystem:__init()
 end
 
 function CollisionBounceSystem:fireEvent(event)
-    if (( object1.__name == "DrawablePolygon" or object1.__name == "Cutie") and (object2.__name == "Cutie" or object2.__name == "DrawablePolygon")) then
-        cutie2.jumpcount = 4
-        if object1.__name == "Cutie" then
-            local cutiexv, cutieyv = object1.body:getLinearVelocity()
-            object1.body:setLinearVelocity(cutiexv, -200)
-        elseif object2.__name == "Cutie" then
-            local cutiexv, cutieyv = object2.body:getLinearVelocity()
-            object2.body:setLinearVelocity(cutiexv, -200)
-        end
-    end"AWESOME CONTACT")
+print(contact)
 end
 

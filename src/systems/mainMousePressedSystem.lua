86caa5c2f50e2f9085fc405db9421d0d92e2b638
Raffomania <raffomania@gmail.com>
2013-05-18 16:26:08 +0200

    fixed dashing direction & speed

--- src/systems/mainMousePressedSystem.lua
+++ src/systems/mainMousePressedSystem.lua
@@ -5,9 +5,9 @@ MainMousePressedSystem = class("MainMousePressedSystem")
 
 function MainMousePressedSystem:fireEvent(event)
     if event.button == "r" and not playercutie:getComponent("DashingComponent") then
-	    local x, y = playercutie:getComponent("PositionComponent").x, playercutie:getComponent("PositionComponent").y
+	    local targetX, targetY = event.x, event.y 
         local xBefore, yBefore = playercutie:getComponent("PhysicsComponent").body:getPosition()
-        playercutie:addComponent(DashingComponent({x=xBefore, y=yBefore}, {x=x, y=y}))
+        playercutie:addComponent(DashingComponent({x=xBefore, y=yBefore}, {x=targetX, y=targetY}))
     elseif event.button == "r" and playercutie:getComponent("DashingComponent") then
         playercutie:removeComponent("DashingComponent")
     end

9728aa771af52f466ade63beabe18407cbc8040e
Raffomania <raffomania@gmail.com>
2013-05-19 23:44:01 +0200

    fixed important bug in collisionSelectSystem

--- src/systems/collisionSelectSystem.lua
+++ src/systems/collisionSelectSystem.lua
@@ -21,8 +21,6 @@ end
 function CollisionSelectSystem:addCollisionAction(component1, component2, object)
     if not self.conditions[component1] then self.conditions[component1] = {} end
     self.conditions[component1][component2] = object
-    if not self.conditions[component2] then self.conditions[component2] = {} end
-    self.conditions[component2][component1] = object
 end
 
 function CollisionSelectSystem:fireEvent(event)
@@ -30,11 +28,11 @@ function CollisionSelectSystem:fireEvent(event)
     local e2 = event.b:getUserData()
 
     for k,v in pairs(e1:getComponents()) do
-        for key,val in pairs(e2:getComponents()) do
+        for k2,val in pairs(e2:getComponents()) do
             if self.conditions[k] then
-                if self.conditions[k][key] then self.conditions[k][key]:action({entity1=e1, entity2=e2}) end
-            elseif self.conditions[key] then
-                if self.conditions[key][k] then self.conditions[key][k]:action({entity1=e2, entity2=e1}) end
+                if self.conditions[k][k2] then self.conditions[k][k2]:action({entity1=e1, entity2=e2}) end
+            elseif self.conditions[k2] then
+                if self.conditions[k2][k] then self.conditions[k2][k]:action({entity1=e2, entity2=e1}) end
             end
         end
     end

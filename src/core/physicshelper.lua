c719da6da5572d71cd2a1dfb826c2502ab3595e8
Raffomania <raffomania@gmail.com>
2013-05-19 16:02:28 +0200

    added a physics helper function

--- src/core/physicshelper.lua
+++ src/core/physicshelper.lua
@@ -1,7 +1,13 @@
 require("core/helper")
 
 function DestroyBody(entity)
-	local physic = entity:getComponent("PhysicsComponent")
-	physic.body:destroy()
+	local physics = entity:getComponent("PhysicsComponent")
+	physics.body:destroy()
 	engine:removeEntity(entity)
+end
+
+function distanceBetween(pos1, pos2)
+    relativeX = pos1[1] - pos2[1]
+    relativeY = pos1[2] - pos2[2]
+    return math.sqrt(relativeX*relativeX + relativeY*relativeY)
 end

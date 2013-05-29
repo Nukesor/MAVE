0ce71b818efdba136a6a3719f1faff3ffd9f201e
Arne Beer <arne@twobeer.de>
2013-05-29 13:50:51 +0200

    ShotCutieCollision fixed,

--- src/collisions/shotCutieCollision.lua
+++ src/collisions/shotCutieCollision.lua
@@ -7,7 +7,7 @@ function ShotCutieCollision:__init()
     self.component2 = "IsShot"
 end
 
-function ShotCutieCollision.action(entities)
+function ShotCutieCollision:action(entities)
 	local entity1 = entities.entity1   
     local entity2 = entities.entity2
     entity1:getComponent("LifeComponent").life = entity1:getComponent("LifeComponent").life - entity2:getComponent("DamageComponent").damage

90e01125f08a49180d7d5caaf9dc8a47cf3387f6
Raffomania <raffomania@gmail.com>
2013-06-28 00:12:49 +0200

    fixed grenade rotation

--- src/systems/weapon/grenadeRotationSystem.lua
+++ src/systems/weapon/grenadeRotationSystem.lua
@@ -12,18 +12,19 @@ function GrenadeRotationSystem:update(dt)
 		-- Rotates the image in direction of movement  .. ?? Horrible english skills
 		if x > 0 then
 			drawable.r = drawable.r + dt*4
-			drawable.ox = -math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-0.75*math.pi)
-		    drawable.oy = math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-1.25*math.pi)
-		else
+			--drawable.ox = -math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-0.75*math.pi)
+		    --drawable.oy = math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-1.25*math.pi)
+		elseif x < 0 then
 			drawable.r = drawable.r - dt*4
-			drawable.ox = math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-0.75*math.pi)
-		    drawable.oy = -math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-1.25*math.pi)
+			--drawable.ox = math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-0.75*math.pi)
+		    --drawable.oy = -math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-1.25*math.pi)
 		end
 		
 		-- Resets the rotation rad, if higher than 2*pi 
 		if (drawable.r > math.pi*2) or (drawable.r < -math.pi*2) then
 			drawable.r = 0
 		end
+		--]]
 	end
 end
 

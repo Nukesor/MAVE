ca8861085d9cfc1a6f817c0deeda8e92dae78c39
Raffomania <raffomania@gmail.com>
2013-06-28 00:48:45 +0200

    adjusted grenade stuff

--- src/systems/weapon/grenadeRotationSystem.lua
+++ src/systems/weapon/grenadeRotationSystem.lua
@@ -9,15 +9,11 @@ function GrenadeRotationSystem:update(dt)
 		local physic = value:getComponent("PhysicsComponent")
 		local x, y = physic.body:getLinearVelocity()
 		
-		-- Rotates the image in direction of movement  .. ?? Horrible english skills
+		-- Rotates the image in direction of movement  .. ?? Horrible english skills // totally correct :D
 		if x > 0 then
-			drawable.r = drawable.r + dt*4
-			--drawable.ox = -math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-0.75*math.pi)
-		    --drawable.oy = math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-1.25*math.pi)
+			drawable.r = drawable.r + dt*4*math.abs(x+y)/35
 		elseif x < 0 then
-			drawable.r = drawable.r - dt*4
-			--drawable.ox = math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-0.75*math.pi)
-		    --drawable.oy = -math.sqrt((drawable.image:getWidth()^2 + drawable.image:getHeight()^2))/2 * math.sin(-drawable.r-1.25*math.pi)
+			drawable.r = drawable.r - dt*4*math.abs(x+y)/35
 		end
 		
 		-- Resets the rotation rad, if higher than 2*pi 

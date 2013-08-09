4e84d53b7bf88d6339df98b7d258a57246ff5ba4
Rafael Epplée <raffomania@gmail.com>
2013-08-09 21:32:36 +0200

    Added new shot graphic

--- src/models/shotmodel.lua
+++ src/models/shotmodel.lua
@@ -9,21 +9,22 @@ function ShotModel:__init(x, y, xt, yt)
     gkat = yt - y
 
     local hypo = math.sqrt(math.pow(gkat, 2) + math.pow(akat, 2))
-    local cos = gkat/hypo
-    local sin = akat/hypo
+    local sin = gkat/hypo
+    local cos = akat/hypo
 
-    local body = love.physics.newBody(world, x+20*sin, y+20*cos, "dynamic")
-    local shape = love.physics.newCircleShape(1) 
+    local body = love.physics.newBody(world, x+(30*cos), y+(30*sin), "dynamic")
+    local shape = love.physics.newRectangleShape(10, 1) 
     local fixture = love.physics.newFixture(body, shape, 0)  
         fixture:setRestitution(1)  
         body:setMass(0)
     self:addComponent(PhysicsComponent(body, fixture, shape ))
     self:addComponent(PositionComponent())
-    self:addComponent(DrawableComponent(resources.images.shot, 0, 1, 1, 5, 5))
+    self:addComponent(DrawableComponent(resources.images.shot, math.atan2(akat, -gkat)-math.pi/2, 1, 1, 5, 5))
     self:addComponent(IsShot())
     self:addComponent(ZIndex(99))
     self:addComponent(DamageComponent(20))
 
     body:setGravityScale(0.1)
-    body:setLinearVelocity((800 * sin), (800 * cos))
+    body:setLinearVelocity((1000*cos), (1000*sin))
+    print(body:getLinearVelocity())
 end
--- src/systems/weapon/mineProximitySystem.lua
+++ src/systems/weapon/mineProximitySystem.lua
@@ -3,7 +3,7 @@ MineProximitySystem = class("MineProximitySystem", System)
 function MineProximitySystem:__init()	end
 
 function MineProximitySystem:update(dt)
-	print(#self.targets)
+	--print(#self.targets)
 	for index, entity in pairs(self.targets) do
 		for index2, enemy in pairs(stack:current().engine:getEntitylist("IsEnemy")) do
 			if distanceBetweenEntities(entity, enemy) <= entity:getComponent("ExplosionComponent").radius then

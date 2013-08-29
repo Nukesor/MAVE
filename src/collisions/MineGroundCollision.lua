c33ad85b4309f3c5cc9ddcba42687f7c677c050f
Arne Beer <arne@twobeer.de>
2013-08-29 16:37:30 +0200

    Mines stop moving, if they touch the ground

---
+++ src/collisions/MineGroundCollision.lua
@@ -0,0 +1,12 @@
+MineGroundCollision = class(MineGroundCollision)
+
+function MineGroundCollision:__init()
+	self.component1 = "IsMine"
+	self.component2 = "DrawablePolygonComponent"
+end
+
+function MineGroundCollision::action(entities)
+    local mine = entities.entity1
+
+    mine:getComponent("PhysicsComponent").body:setLinearVelocity(0, 0)
+end

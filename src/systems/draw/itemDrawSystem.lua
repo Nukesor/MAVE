8d48b94a43ab9cf679446135766b3948d978d785
Arne Beer <arne@twobeer.de>
2013-08-31 00:09:29 +0200

    Bugs fixed
    - itemDrawSystem doesnt crash anymore, if item isn't loaded yet
    - lifebar isn't super long anylonger, if life is negative

--- src/systems/draw/itemDrawSystem.lua
+++ src/systems/draw/itemDrawSystem.lua
@@ -3,21 +3,23 @@ ItemDrawSystem = class("ItemDrawSystem", System)
 -- Makes cuties carry the currently selected item
 function ItemDrawSystem:draw() 
     for key, entity in pairs(self.targets) do
-        local item = entity:getComponent("ItemComponent")
-        local drawable = item.drawableEntity
-        local mouseX, mouseY = love.mouse.getPosition()
-        local entityX = entity:getComponent("PositionComponent").x
-        local entityY = entity:getComponent("PositionComponent").y
-        local x = mouseX - entityX
-        local y = mouseY - entityY
+        if entity:getComponent("ItemComponent") then
+            local item = entity:getComponent("ItemComponent")
+            local drawable = item.drawableEntity
+            local mouseX, mouseY = love.mouse.getPosition()
+            local entityX = entity:getComponent("PositionComponent").x
+            local entityY = entity:getComponent("PositionComponent").y
+            local x = mouseX - entityX
+            local y = mouseY - entityY
 
-        local sy = item.sy
-        if x < 0 then
-            sy = -sy
+            local sy = item.sy
+            if x < 0 then
+                sy = -sy
+            end
+            
+            love.graphics.setColor(255,255,255)
+            love.graphics.draw(item.image, entityX, entityY, math.atan2(x,-y)-(math.pi/2), item.sx, sy, 0, 0)
         end
-        
-        love.graphics.setColor(255,255,255)
-        love.graphics.draw(item.image, entityX, entityY, math.atan2(x,-y)-(math.pi/2), item.sx, sy, 0, 0)
     end
 end
 

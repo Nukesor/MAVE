ca1b88412ee80c6f8c8247405f15a3dc67b4a18b
Arne Beer <arne@twobeer.de>
2013-09-16 02:53:24 +0200

    GoldUpDisplaySystem improved
        - System now removes the entity before transparency is full again.
        - Color and timing adjusted
        - Slowmotion Problem removed

--- src/systems/draw/goldUpDisplaySystem.lua
+++ src/systems/draw/goldUpDisplaySystem.lua
@@ -2,7 +2,7 @@ GoldUpDisplaySystem = class("GoldUpDisplaySystem", System)
 
 function GoldUpDisplaySystem:draw()
 	for index, value in pairs(self.targets) do
-		if value:getComponent("TimerComponent").time < 0 then
+		if value:getComponent("TimerComponent").time < 0 or value:getComponent("GoldUpComponent").transparency < 0 then
 			stack:current().engine:removeEntity(value)
 		else
 			local position = value:getComponent("PositionComponent")
@@ -10,7 +10,7 @@ function GoldUpDisplaySystem:draw()
 			love.graphics.setFont(resources.fonts.twenty)
 			love.graphics.print("+ " .. value:getComponent("GoldUpComponent").gold, position.x, position.y-20)
 			value:getComponent("PositionComponent").y = value:getComponent("PositionComponent").y - 1
-			value:getComponent("GoldUpComponent").transparency =  value:getComponent("GoldUpComponent").transparency - 2
+			value:getComponent("GoldUpComponent").transparency =  value:getComponent("GoldUpComponent").transparency - 10
 		end
 	end
 end
